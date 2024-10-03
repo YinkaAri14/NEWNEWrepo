4240243d7512c1add46938a213fe85f3609ff434
YinkaAri14 <aariwa2@lsu.edu>
2024-10-03 10:57:24 -0500

    javascript1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 // JS scripts placed here
+
+const myButton = document.getElementById("intro")
+const myButton2 = document.getElementById("submit")
+console.log(myButton);
+console.log(myButton2)
+
+const element = document.getElementsByTagName("p")
+console.log(element)
+
+const x = document.querySelectorAll(".logo")
+console.log(x);
+
+const y = document.querySelector(".logo")
+console.log(y);
+
+
+const nameInput = document.getElementById('name')
+const nameValue = nameInput.value;
+console.log('nameInput:', nameInput);
+console.log('nameValue', nameValue);
+alert('You clicked me!,'+ nameValue);
